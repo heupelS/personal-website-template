d4e3a3a3ed9b87675034e0f72e8056321754e8a5
heupelS <simeon@heupel.de>
2023-04-07 22:30:57 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./styles/variables.css";
+import { ToggleColorMode } from "./components/themes/theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ToggleColorMode>
+      <div className="App">
+        <header className="App-header"></header>
+        <button>test</button>
+      </div>
+    </ToggleColorMode>
   );
 }
 
