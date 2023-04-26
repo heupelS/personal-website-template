be1b6a8906f6ce42c40125b40b7263b99bba1386
heupelS <simeon@heupel.de>
2023-04-26 23:50:11 +0200

    timeline added to main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,16 +8,20 @@ import ThemeToggler from "../components/themes/ThemeToggler";
 import { Parallax } from "@react-spring/parallax";
 import Button from "@mui/material/Button";
 import HeaderBar from "../components/content/Headerbar";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Content from "../components/content/Content";
 import SocialIcons from "../components/content/Socials";
+import CustomizedTimeline from "../components/timeline";
 
 export default function Main() {
   return (
     <>
-      <ThemeToggler></ThemeToggler>
-      <Content></Content>
+      <Container sx={{ height: "100vh" }}>
+        <ThemeToggler></ThemeToggler>
+        <Content></Content>
+      </Container>
       <SocialIcons></SocialIcons>
+      <CustomizedTimeline id="Experience"></CustomizedTimeline>
       {/* <SlidingLayers></SlidingLayers> */}
     </>
   );
