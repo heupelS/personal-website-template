c437a0567465e5a30698e43730b3e65a1c3d00ab
heupelS <simeon@heupel.de>
2023-05-18 18:29:23 +0200

    adjusted timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -12,6 +12,9 @@ import Box from "@mui/material/Box";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { ThemeContext } from "@emotion/react";
 
 export default function CustomizedTimeline() {
   const isMobile = useMediaQuery("(max-width:600px)");
@@ -31,26 +34,60 @@ export default function CustomizedTimeline() {
           {workitem.startDate.slice(0, 7)}
         </TimelineOppositeContent>
         <TimelineSeparator>
-          {!isFirstItem && <TimelineConnector />}
-          <TimelineDot />
-          {!isLastItem && <TimelineConnector />}
+          {isFirstItem ? (
+            <TimelineConnector style={{ position: "relative" }}>
+              <ArrowForwardIosIcon
+                style={{
+                  color: "inherit",
+                  position: "absolute",
+                  right: "40%",
+                  top: "5%",
+                  transform: "translate(50%, -50%) rotate(-90deg)",
+                }}
+              />
+            </TimelineConnector>
+          ) : (
+            <TimelineConnector />
+          )}
+          <TimelineDot variant="outlined" color="primary">
+            <i class={workitem.icon} style={{ color: "inherit" }}></i>
+          </TimelineDot>
+          <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
-          <Typography
-            variant="h6"
-            component="span"
-            sx={{
-              fontSize: {
-                xs: "1rem",
-                sm: "1.2rem",
-                md: "1.5rem",
-                lg: "2rem",
-              },
-            }}
-          >
-            {workitem.company}
-          </Typography>
-          <Typography>{workitem.position}</Typography>
+          <Paper sx={{ mx: 2, mt: 2, mb: 2, textAlign: "left" }}>
+            <Box sx={{ marginLeft: "1vw" }}>
+              <Typography
+                variant="h6"
+                component="span"
+                sx={{
+                  fontSize: {
+                    xs: "1rem",
+                    sm: "1.2rem",
+                    md: "1.5rem",
+                    lg: "2rem",
+                  },
+                }}
+              >
+                {workitem.company}
+              </Typography>
+              <Typography>{workitem.position}</Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{
+                  fontSize: {
+                    xs: "1rem",
+                    sm: "1.2rem",
+                    md: "1.5rem",
+                    lg: "2rem",
+                  },
+                }}
+              >
+                {workitem.startDate.slice(0, 7)}
+              </Typography>
+            </Box>
+          </Paper>
         </TimelineContent>
       </TimelineItem>
     );
@@ -63,9 +100,25 @@ export default function CustomizedTimeline() {
     return (
       <TimelineItem key={workitem.company}>
         <TimelineSeparator>
-          {!isFirstItem && <TimelineConnector />}
-          <TimelineDot />
-          {!isLastItem && <TimelineConnector />}
+          {isFirstItem ? (
+            <TimelineConnector style={{ position: "relative" }}>
+              <ArrowForwardIosIcon
+                style={{
+                  color: "inherit",
+                  position: "absolute",
+                  right: "40%",
+                  top: "5%",
+                  transform: "translate(50%, -50%) rotate(-90deg)",
+                }}
+              />
+            </TimelineConnector>
+          ) : (
+            <TimelineConnector />
+          )}
+          <TimelineDot variant="outlined" color="primary">
+            <i class={workitem.icon} style={{ color: "inherit" }}></i>
+          </TimelineDot>
+          <TimelineConnector />
         </TimelineSeparator>
         <Paper sx={{ mx: 2, mt: 2, mb: 2 }}>
           <TimelineContent>
@@ -105,12 +158,22 @@ export default function CustomizedTimeline() {
   });
 
   return (
-    <Box>
+    <Box sx={{ mx: "auto", mt: 14 }}>
+      <Typography variant="h3" gutterBottom sx={{ textAlign: "center" }}>
+        Experience
+      </Typography>
+      <Divider
+        sx={{
+          width: "40%",
+          margin: "0 auto",
+          height: "3px",
+          borderBottomWidth: "2px",
+        }}
+      />
+
       <Grid container justifyContent="center">
         {!isMobile && (
-          <Timeline position="alternate" n id="experience-web">
-            {TimelineWorkItemsWeb}
-          </Timeline>
+          <Timeline position="alternate">{TimelineWorkItemsWeb}</Timeline>
         )}
         {isMobile && (
           <Timeline
@@ -121,11 +184,12 @@ export default function CustomizedTimeline() {
               },
             }}
             position="right"
-            id="experience-mobile"
           >
             {TimelineWorkItemsMobile}
           </Timeline>
         )}
+
+        <Typography variant="h4">{Resume.work.name}</Typography>
       </Grid>
     </Box>
   );
