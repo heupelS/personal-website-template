1dc61906c1c7a010f453d9030406b93c6b619c3f
heupelS <simeon@heupel.de>
2023-05-23 22:53:20 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./styles/variables.css";
 import { ToggleColorMode } from "./components/themes/ThemeProvider";
-import Footer from "./pages/Footer";
 import AppRouter from "./AppRouter";
 
 function App() {
