bdb3508cac3c2b485e1f93b004a7fd5c062e6984
heupelS <simeon@heupel.de>
2023-05-29 15:49:36 +0200

    fixed code

--- src/components/themes/ThemeBackground.js
+++ src/components/themes/ThemeBackground.js
@@ -1,14 +1,15 @@
 import { useTheme } from "@mui/material/styles";
-import { useMediaQuery } from "@mui/material";
 import * as React from "react";
 
 export const MetaThemeColor = () => {
   const theme = useTheme();
-  const metaThemeColor = document.querySelector("meta[name=theme-color]");
 
   React.useEffect(() => {
-    metaThemeColor.setAttribute("content", theme.palette.background.default);
-  }, [theme.palette.background.default, metaThemeColor]);
+    const metaThemeColor = document.querySelector("meta[name=theme-color]");
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute("content", theme.palette.background.default);
+    }
+  }, [theme.palette.background.default]);
 
   return null;
 };
