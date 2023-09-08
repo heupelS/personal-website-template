1ac7a4930d0a5121f2f54a265ae6f5a6815c9c3b
heupelS <simeon@heupel.de>
2023-09-08 20:48:02 +0200

    clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
