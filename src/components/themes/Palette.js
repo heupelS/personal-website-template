05b5385898c92a75fa14a000b4fe9813cd09cbed
heupelS <simeon@heupel.de>
2023-09-18 22:59:13 +0200

    font update and coloring

--- src/components/themes/Palette.js
+++ src/components/themes/Palette.js
@@ -14,7 +14,7 @@ export const getDesignTokens = (mode) =>
               background: {
                 default: "#ffffff",
                 paper: "#fff",
-                infobox: "#ffffff",
+                infobox: "#fafafa",
                 gradient: `linear-gradient(to right, #2C3A8B 0%, 10.203412073490814%, #294495 20.406824146981627%, 30.183727034120736%, #1E5DAA 39.960629921259844%, 49.86876640419948%, #0E89D0 59.7769028871391%, 70.11154855643045%, #04A3E5 80.4461942257218%, 90.2230971128609%, #01ADEE 100%);`,
               },
               text: {
@@ -29,7 +29,7 @@ export const getDesignTokens = (mode) =>
               background: {
                 default: grey,
                 paper: "#141313",
-                infobox: "#141313",
+                infobox: grey[900],
                 gradient: `linear-gradient(to right, #2C3A8B 0%, 10.203412073490814%, #294495 20.406824146981627%, 30.183727034120736%, #1E5DAA 39.960629921259844%, 49.86876640419948%, #0E89D0 59.7769028871391%, 70.11154855643045%, #04A3E5 80.4461942257218%, 90.2230971128609%, #01ADEE 100%);`,
               },
               text: {
@@ -44,17 +44,29 @@ export const getDesignTokens = (mode) =>
     {
       typography: {
         fontSize: 16,
+        fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
         htmlFontSize: 16,
         h2: {
           fontWeight: 500,
         },
+        h4: { fontWeight: "bold", fontSize: 36 },
         h5: {
           fontWeight: 500,
+          fontFamily: "Courier New, monospace",
+        },
+        h6: {
           fontFamily: "Roboto Mono, monospace",
+          fontWeight: 100,
+          fontSize: 16,
+          textDecoration: "underline",
         },
         body1: {
           fontWeight: 500,
-          fontFamily: "Roboto Mono, monospace",
+        },
+        body2: {
+          textOverflow: "ellipsis",
+          wordWrap: "break-word",
+          marginBottom: "2vh",
         },
       },
       components: {
@@ -86,7 +98,6 @@ export const getDesignTokens = (mode) =>
             root: ({ theme }) => ({
               marginBottom: theme.spacing(2),
               color: theme.palette.text.primary,
-              backgroundColor: "transparent",
               background: "inherit",
               "@media (hover: hover)": {
                 "&:hover": {
@@ -103,7 +114,6 @@ export const getDesignTokens = (mode) =>
           styleOverrides: {
             root: ({ theme }) => ({
               marginBottom: theme.spacing(2),
-              backgroundColor: "transparent",
               boxShadow:
                 "0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)",
               color: "inherit",
@@ -133,6 +143,22 @@ export const getDesignTokens = (mode) =>
           styleOverrides: {
             root: ({ theme }) => ({
               backgroundImage: theme.palette.background.gradient,
+            }),
+          },
+        },
+        MuiTableCell: {
+          styleOverrides: {
+            root: ({ theme }) => ({
+              borderBottom: "none",
+            }),
+          },
+        },
+        MuiAvatar: {
+          styleOverrides: {
+            root: ({ theme }) => ({
+              border: `2px solid ${theme.palette.primary.main}`,
+              background: "inherit",
+              marginRight: "5vw",
             }),
           },
         },
