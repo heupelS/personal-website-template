67021bff9dd4ae1dbe0109cc039dc1337403cdd1
heupelS <simeon@heupel.de>
2024-01-17 16:41:29 +0100

    added insights

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./styles/variables.css";
 import { ToggleColorMode } from "./components/themes/ThemeProvider";
 import AppRouter from "./AppRouter";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <ToggleColorMode>
       <CssBaseline enableColorScheme />
       <AppRouter></AppRouter>
+      <SpeedInsights />
     </ToggleColorMode>
   );
 }
